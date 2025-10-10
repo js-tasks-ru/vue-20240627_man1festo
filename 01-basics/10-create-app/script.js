fc760f8fb0feb36d46c137f7dab1d97ac2e11723
man1festo <man1festo@mail.ru>
2025-10-10 11:49:04 +0300

    2nd task : create app and 3rd task: weather

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,26 @@
 import { defineComponent, createApp } from 'vue'
+const App = defineComponent({
+  name: 'App',
+  setup() {
+    const dateFormatted = new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' });
+    return {
+      dateFormatted
+    };
+  },
+
+  template: `
+  <div>{{'Сегодня ' + dateFormatted}}</div>
+  `
+});
+// Создаём Vue приложение - абстракцию для конфигурации
+const app = createApp(App)
+// Через приложение можно устанавливать плагины, глобальную конфигурацию и др.
+// app.use(plugin)
+
+// После монтирования приложения на страницу, Vue отвечает за рендеринг этой части страницы.
+// Метод возвращает экземпляр корневого компонента
+const vm = app.mount('#app')
+
+// Для отладки - добавим в глобальную переменную
+// Позже научимся делать это через vue-devtools
+window.vm = vm
